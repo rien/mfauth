ba7aad5d8a02b2856d277af8d3a3f73edeed4f77
Rien Maertens <rien.maertens@posteo.be>
2021-09-26 17:21:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MFAuth
+
+~~Mother F~~ Multi Factor Authentication for mail clients
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Simple OAuth2 client for mail clients";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {self,  nixpkgs, flake-utils, rust-overlay, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    overlays = [ (import rust-overlay) ];
+    pkgs = import nixpkgs {
+      inherit system overlays;
+    };
+  in
+  with pkgs;
+  {
+    devShell = mkShell {
+      buildInputs = [
+        rust-bin.stable.latest.default
+      ];
+    };
+  });
+}
